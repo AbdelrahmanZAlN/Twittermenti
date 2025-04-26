a613b707f1f4b04039a3fd516adb1d1621e98a85
Abdelrahman Zain <boody.ali.zean@gmail.com>
2025-04-26 08:21:20 +0300

    Initial Commit

---
+++ Twittermenti/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Twittermenti
+//
+//  Created by Abdelrahman Zain on 26/04/2025.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
